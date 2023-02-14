e616ba9638c595df47eb40778376a8e7e868e9ba
kmomozin <khinmomozin@gmail.com>
2023-02-13 21:13:16 -0700

    update points with provided data queries and route setup

--- db/queries/points-queries.js
+++ db/queries/points-queries.js
@@ -49,16 +49,40 @@ const addPoints = (body) => {
 };
 
 const updatePoint = (pointId, body) => {
+  let updateStatement = "";
+
+  if (body.title) {
+    updateStatement += `title = '${body.title}',`;
+  }
+  if (body.description) {
+    updateStatement += `description = '${body.description}',`;
+  }
+  if (body.address_line_1) {
+    updateStatement += `address_line_1 = '${body.address_line_1}',`;
+  }
+  if (body.address_line_2) {
+    updateStatement += `address_line_2 = '${body.address_line_2}',`;
+  }
+  if (body.lat) {
+    updateStatement += `lat = ${body.lat},`;
+  }
+  if (body.lon) {
+    updateStatement += `lon = ${body.lon},`;
+  }
+  if (body.image_url) {
+    updateStatement += `image_url = '${body.image_url}',`;
+  }
+
+  updateStatement = updateStatement.slice(0, -1);
+
   const query = `
   UPDATE points
-  SET title = $1, description = $2
-  WHERE points.id = $3
+  SET ${updateStatement}
+  WHERE points.id = $1
   RETURNING *;
   `;
 
-  const param = [body.title, body.description, pointId];
-
-  return db.query(query, param)
+  return db.query(query, [pointId])
     .then(data => {
       return data.rows;
     })
--- routes/points-router.js
+++ routes/points-router.js
@@ -6,18 +6,18 @@ const pointQueries = require('../db/queries/points-queries');
 //change this to get body from req.body and userId from cookie
 //check if map_id need to be passed from route or inside the object?
 router.post('/new', (req, res) => {
-  const pointId = req.params.pointId;
   const body = {
     title: "New Hike",
-    // description: "New Hike Description",
+    description: "New Hike Description",
     address_line_1: "Line 1",
     address_line_2: "Line 2",
     lat: 50.96329262987205,
     lon: -115.13735273275209,
     map_id: 3,
-    // image_url: 'https://cdn.pixabay.com/photo/2016/06/11/05/20/maligne-lake-1449489_1280.jpg'
+    image_url: 'https://cdn.pixabay.com/photo/2016/06/11/05/20/maligne-lake-1449489_1280.jpg'
   };
-  pointQueries.addPoints(pointId, body)
+  console.log("body in route:", body);
+  pointQueries.addPoints(body)
     .then(map => {
       res.json({ map });
     })
