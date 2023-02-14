69457d9bf9a59da43f50b0c0d288cbf5c73da8de
kmomozin <khinmomozin@gmail.com>
2023-02-13 20:49:38 -0700

    Add new point by mapId queries and route setup

--- db/queries/points-queries.js
+++ db/queries/points-queries.js
@@ -16,7 +16,40 @@ const getPointsDetailsByMapId = (mapId) => {
     });
 };
 
+const addPoints = (pointId, body) => {
+
+  let columnName = "title, address_line_1, address_line_2, lat, lon, map_id,";
+  let columnValue = `'${body.title}','${body.address_line_1}','${body.address_line_2}',${body.lat},${body.lon},${body.map_id},`;
+
+  if (body.description) {
+    columnName += "description,";
+    columnValue += `'${body.description}',`;
+  }
+  if (body.image_url) {
+    columnName += "image_url,";
+    columnValue += `'${body.image_url}',`;
+  }
+
+  columnName = columnName.slice(0, -1);
+  columnValue = columnValue.slice(0, -1);
+
+  const query = `
+  INSERT INTO points (${columnName})
+  VALUES (${columnValue})
+  RETURNING *;
+  `;
+
+  return db.query(query)
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      return err;
+    });
+};
+
 
 module.exports = {
   getPointsDetailsByMapId,
+  addPoints,
 };
--- routes/points-router.js
+++ routes/points-router.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const pointQueries = require('../db/queries/points-queries');
 
+
+//change this to get body from req.body and userId from cookie
+router.post('/new', (req, res) => {
+  const pointId = req.params.pointId;
+  const body = {
+    title: "New Hike",
+    // description: "New Hike Description",
+    address_line_1: "Line 1",
+    address_line_2: "Line 2",
+    lat: 50.96329262987205,
+    lon: -115.13735273275209,
+    map_id: 3,
+    // image_url: 'https://cdn.pixabay.com/photo/2016/06/11/05/20/maligne-lake-1449489_1280.jpg'
+  };
+  pointQueries.addPoints(pointId, body)
+    .then(map => {
+      res.json({ map });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
 
 module.exports = router;
