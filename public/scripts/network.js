d13ffc2ded4231f6b2c3725997fadea2228dfc24
kmomozin <khinmomozin@gmail.com>
2023-02-14 17:32:37 -0700

    add post,delete routes to client side routes in network file

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -52,8 +52,54 @@ function getMapDetails(mapId) {
   });
 }
 
+function addNewMap(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/maps/new",
+    data
+  });
+}
+
+function updateMap(mapId, data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/maps/${mapId}`,
+    data
+  });
+}
+
+function deleteMap(mapId, data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/maps/${mapId}/delete`,
+    data
+  });
+}
 
 // POINT RELATED ROUTES //
+
+function addNewPoint(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/points/new",
+    data
+  });
+}
+
+function updatePoint(pointId, data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/points/${pointId}`,
+    data
+  });
+}
+
+function deletePoint(pointId) {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/points/${pointId}`
+  });
+}
 
 // FAVORUITES MAP RELATED ROUTES //
 
@@ -71,11 +117,39 @@ function getContributors(mapId) {
   });
 }
 
+function addContributorToMap(mapId) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/contributions/${mapId}`
+  });
+}
+
+function removeContributorFromMap(mapId) {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/contributions/${mapId}`
+  });
+}
+
 // CONTRIBUTED MAP RELATED ROUTES //
 
 function getMyFavouriteMaps() {
   return $.ajax({
     method: "GET",
     url: "/api/favourites/"
+  });
+}
+
+function addMapToFavourites(mapId) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/favourites/${mapId}`
+  });
+}
+
+function removeMapFromFavourites(mapId) {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/favourites/${mapId}`
   });
 }
