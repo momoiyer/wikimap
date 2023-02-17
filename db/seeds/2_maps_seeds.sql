a272f02bf2f2536e449ebddc378eeb4087210c0a
Kat Nishimura <katherine.nishimura@live.ca>
2023-02-17 05:28:35 +0000

    seeds

--- db/seeds/2_maps_seeds.sql
+++ db/seeds/2_maps_seeds.sql
@@ -1,8 +1,18 @@
 INSERT INTO maps
 ( title, description, owner_id)
 VALUES
-('The Last of Us','Filming Locations around Alberta',3),
-('Favourite Hikes','The best hiking spots in Kananaskis',15),
-('Best Cafe around town','Best Cafe in Vancouver',10),
+('The Last of Us','Filming Locations around Alberta.',3),
+('Favourite Hikes','The best hiking spots in Kananaskis.',15),
+('Best Cafe in Vancouver','All the places to get great coffee when it is raining!',10),
 ('Winnipeg Cafes', 'All of my favourite coffee shops to visit when I am in Winnipeg.',8),
-('Best Birding','Places to checkout for next bird watching adventures',8);
+('Birding Spots', 'Next places to go bird watching in the spring',8),
+('Best Waterfalls in BC','Photo opportunities from our roadtrip.',8),
+('Fave Dog Parks','Places to take my dog Ringo for his next outing.',10),
+('Vancouver Landmarks','Tourist traps, but fun places to take my parents when they visit',8),
+('Fave Bakeries in Vancouver','Need to try each to determine who is number one!',8),
+('Vintage/Thrift Shops','Remember to check the spots our next shopping day.',8),
+('Outdoor Pools','PLaces to go during the next heat dome',10);
+
+
+
+
--- db/seeds/3_points_seeds.sql
+++ db/seeds/3_points_seeds.sql
@@ -11,23 +11,45 @@ VALUES
 INSERT INTO points
 (title, description,address_line_1,address_line_2,lat,lon,map_id,image_url)
 VALUES
-('Troll Falls','Easy trail of 5km hike','Hummingbird Plume Lookout Trail','Kananaskis Improvement District, AB, Canada',50.96329262987205, -115.13735273275209,2,'https://cdn.pixabay.com/photo/2016/06/11/05/20/maligne-lake-1449489_1280.jpg');
+('Troll Falls','Easy trail of 5km hike.','Hummingbird Plume Lookout Trail','Kananaskis Improvement District, AB, Canada',50.96329262987205, -115.13735273275209,2,'https://cdn.pixabay.com/photo/2016/06/11/05/20/maligne-lake-1449489_1280.jpg');
 
 INSERT INTO points
 (title, description,address_line_1,address_line_2,lat,lon,map_id)
 VALUES
-('Little Lougheed','Hard trail of 5km hike','Wind Ridge Tr','Town of Canmore, AB T1W 2W4, Canada',51.018693965427225, -115.30335032182762,2),
-('Sparrowhawk Tarns','Moderate trail of 11km hike','The Orphan Route','Kananaskis Improvement District, AB T1W 2V3, Canada',51.00153291347325, -115.32276809423867,2);
+('Little Lougheed','Hard trail of 5km hike.','Wind Ridge Tr','Town of Canmore, AB T1W 2W4, Canada',51.018693965427225, -115.30335032182762,2),
+('Sparrowhawk Tarns','Moderate trail of 11km hike.','The Orphan Route','Kananaskis Improvement District, AB T1W 2V3, Canada',51.00153291347325, -115.32276809423867,2);
 
 
 INSERT INTO points
 (title, description,address_line_1,address_line_2,lat,lon,map_id,image_url)
 VALUES
-('Deer Lake Park','Nice trails and great views, a good place to have a picnic','6588 Deer Lake Ave','Burnaby, BC, V5G 3T7, Canada',49.23834, -122.96891,5, 'https://images.unsplash.com/photo-1587667659239-68adcadc2f2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
-('Blackie Spit','Lots of waterfowl and sometimes raptors','3136 McBride Ave','Surrey, BC, V4A 3E2, Canada',49.05882, -122.87911,5, 'https://www.mrussellphotography.com/photoblog/photography/2015/crescent-beach/blackie-spit-crescent-beach-33134.jpg'),
+('Deer Lake Park','Nice trails and great views, a good place to have a picnic.','6588 Deer Lake Ave','Burnaby, BC, V5G 3T7, Canada',49.23834, -122.96891,5, 'https://images.unsplash.com/photo-1587667659239-68adcadc2f2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+('Blackie Spit','Lots of waterfowl and sometimes raptors.','3136 McBride Ave','Surrey, BC, V4A 3E2, Canada',49.05882, -122.87911,5, 'https://www.mrussellphotography.com/photoblog/photography/2015/crescent-beach/blackie-spit-crescent-beach-33134.jpg'),
 ('Cafe Postal','Best spot in St.Boniface, maybe walk along the river with your coffee.','202 Provencher Blvd','Winnipeg, MB, R2H 0G3, Canada',49.89229, -97.12097,4, 'https://i.pinimg.com/originals/65/d8/6f/65d86f8aee5f30223e5c526a814add36.jpg'),
-('Thom Bargen Corydon','Realy cute interior, they also serve coffee cocktails','743 Corydon Ave','Winnipeg, MB, R3M 0W5, Canada',49.87268, -97.15266,4, 'https://cdn.shopify.com/s/files/1/0372/0999/7450/files/2_DSC4988_400x.jpg?v=1666897697'),
-('Little Sister Coffee Maker, South Osborne','Beautiful interior design, great in the summer for their lavendar soft serve','539 Osborne St','Winnipeg, MB, Canada',49.86602, -97.13491,4,'https://lh5.googleusercontent.com/p/AF1QipMXgx4mcXtTk2sM9t3Pl1GuAgiMz9I5krU6IbHW=w141-h101-n-k-no-nu');
+('Thom Bargen Corydon','Realy cute interior, they also serve coffee cocktails.','743 Corydon Ave','Winnipeg, MB, R3M 0W5, Canada',49.87268, -97.15266,4, 'https://cdn.shopify.com/s/files/1/0372/0999/7450/files/2_DSC4988_400x.jpg?v=1666897697'),
+('Little Sister Coffee Maker, South Osborne','Beautiful interior design, great in the summer for their lavender soft serve.','539 Osborne St','Winnipeg, MB, Canada',49.86602, -97.13491,4,'https://lh5.googleusercontent.com/p/AF1QipMXgx4mcXtTk2sM9t3Pl1GuAgiMz9I5krU6IbHW=w141-h101-n-k-no-nu'),
 
 
+('Keyhole Falls','The beautiful Keyhole Falls are just a couple kilometres upriver from Keyhole Hot Springs.','V0N 2L2','Squamish-Lillooet, BC, Canada',50.68341, -123.43140,6,'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Keyhole_Falls.jpg/330px-Keyhole_Falls.jpg'),
+('Dawson Falls','Scenic waterfall that looks like a miniature Niagara Falls.','Myrtle Point, BC','V0E 1J0, Canada',51.96508, -120.12492,6,'https://lh5.googleusercontent.com/p/AF1QipMXgx4mcXtTk2sM9t3Pl1GuAgiMz9I5krU6IbHW=w141-h101-n-k-no-nu'),
+('Shannon Falls','The 3rd highest waterfall in BC, dog friendly!','BC-97','Squamish-Lillooet D, BC, V0N 1T0, Canada',49.66558, -123.16217,6,'https://lh3.googleusercontent.com/gps-proxy/ALm4wwn_oy9U47_kLYgxXcKhDqdenjfW8IPo_zE0F2u-5Ee8EjCkfl-2mVM6poeBSCQN4HxFs5zKLYuJhKMDSJvdcnEVqW0qUQIcStcvwQL25AC5EDDYz2WEbn3v0sZeqzQ90HrW4SOmdNOGK1ya-n4cR8bJGXHROSun3c5At_FmM_niL80215JJB529bA=w408-h271-k-no'),
+
+
+('Charleson Dog Park','Cute dog park right on the water!','False Creek','Vancouver, BC, Canada',49.266897, -123.124551,7,'https://images.dailyhive.com/20170119090248/Charleson-Park-Instagram-Vancouver-Corgi.jpg'),
+('Buntzen Lake Recreation Area','Half hour drive from the city, but so beautiful. Swimming area for the dogs!','5000 Sunnyside Rd','Anmore, BC, V3H4Z2, Canada',49.33936, -122.85883,7,'https://images.dailyhive.com/20170119090225/Buntzen-Lake-dog-beach-Yelp.jpg'),
+('Queen Elizabeth Dog Park','Not fenced, but great for playing frisbee','227-281 W 37th Ave','Vancouver, BC, Canada',49.23788, -123.11100,7,'https://s3-media0.fl.yelpcdn.com/bphoto/hK7jUYxx1u0TqfnQufrxjQ/348s.jpg'),
+
+('Science World','Wanna try Science World After Dark!','1455 Quebec St','Vancouver, BC, Canada',49.27335, -123.10383,8,'https://upload.wikimedia.org/wikipedia/commons/e/e5/Science_World_at_TELUS_World_of_Science.jpg'),
+('Lions Gate Bridge','Fun to drive over, as long as it is not rush hour!','Lions Gate Bridge Rd','Vancouver, BC, Canada',49.31469, -123.13924,8,'https://upload.wikimedia.org/wikipedia/commons/5/5c/LionsGateBridgeTidepoolReflection.jpg'),
+('Canada Place','Best to visit right before sunset and walk along the seawall afterwards','999 Canada Pl','Vancouver, BC, V6C3T4, Canada',49.28883, -123.11118,8,'https://lh5.googleusercontent.com/p/AF1QipMXgx4mcXtTk2sM9t3Pl1GuAgiMz9I5krU6IbHW=w141-h101-n-k-no-nu'),
+
+('Livia Sweets','Everything about this place is magical, except waiting for a table at weekend brunch.','1399 Commercial Dr','Vancouver, BC, Canada',49.272265, -123.069725,9,'https://images.dailyhive.com/20190111104114/IMG_0202.jpg'),
+('Nemesis Coffee','Really cool building, friendly staff. The pistachio swirl is to die for!','555 Great Northern Way','Vancouver, BC, Canada',49.26705, -123.09258,9,'https://i0.wp.com/eatswithmo.com/wp-content/uploads/2021/06/20210619_084240-min-scaled.jpg?fit=2560%2C1440&ssl=1'),
+('Breka Hastings','So convenient, open 24 hours! The apple fritter is the best.','740 W Hastings Street','Vancouver, BC, Canada',49.28575, -123.11465,9,'https://s3-media0.fl.yelpcdn.com/bphoto/4J-BJVkNZI5i0P4xxtg1Ag/348s.jpg'),
+
+('Front & Company','Always find a lot of great stuff here.','3772 Main St','Vancouver, BC, V5V 3N7, Canada',49.25140, -123.10080,10,'https://www.bcliving.ca/media/image/99195_max.jpg'),
+('Hunter & Hare','Have to take the seabus but it is worth it!','225 Lonsdale Ave','North Vancouver, BC, V7M 2E9, Canada',49.31320, -123.07781,10,'https://www.google.com/maps/place/Hunter+%26+Hare/@49.3130567,-123.0775342,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNyfA0_TKlyBNe3Zo5iZkj2GFByvzVKGQaoRKtW!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNyfA0_TKlyBNe3Zo5iZkj2GFByvzVKGQaoRKtW%3Dw114-h86-k-no!7i4032!8i3024!4m7!3m6!1s0x5486715782a5f83f:0x9d77ab9a3d0c4eec!8m2!3d49.3131748!4d-123.0778105!14m1!1BCgIgARICEAE?hl=en#'),
+
+('Second Beach Swimming Pool','For when you wanna see the ocean but dont want to be in it!','Stanley Park Dr','Vancouver, BC, Canada',49.29553, -123.15155,11,'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.insidevancouver.ca%2F2016%2F07%2F07%2Foutdoor-pools-in-the-city%2F&psig=AOvVaw0zl8wnyKio1Swpf9j45MEk&ust=1676697377244000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNjA2M7mm_0CFQAAAAAdAAAAABAE'),
+('Hillcrest Aquatic Center','One of the best community centers around.','4575 Clancy Loranger Way','Vancouver, BC, Canada',49.24396, -123.10760,11,'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.insidevancouver.ca%2F2010%2F07%2F28%2Fvancouvers-largest-aquatic-centre-opens-july-31-2010%2F&psig=AOvVaw0qjZif_0iqw-1A_wGrS0Z4&ust=1676697343175000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJiHs77mm_0CFQAAAAAdAAAAABAJ');
 
